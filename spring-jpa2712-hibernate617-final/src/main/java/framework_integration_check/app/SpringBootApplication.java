e9a14b7a77fdab5d7f2ac32dc3deaa02e9eae306
internetms52 <internetms52@gmail.com>
2023-09-20 21:43:58 +0800

    加入顯示訊息

--- spring-jpa2712-hibernate617-final/src/main/java/framework_integration_check/app/SpringBootApplication.java
+++ spring-jpa2712-hibernate617-final/src/main/java/framework_integration_check/app/SpringBootApplication.java
@@ -1,11 +1,14 @@
 package framework_integration_check.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
-
+	private static Logger logger = LoggerFactory.getLogger(SpringBootApplication.class);
 	public static void main(String[] args) {
+		logger.info("spring-jpa2712-hibernate617-final");
 		SpringApplication.run(SpringBootApplication.class, args);
 	}
 
