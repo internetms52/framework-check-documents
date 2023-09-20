74e25a476628e86862db56940ebcdf4adedf7451
internetms52 <internetms52@gmail.com>
2023-09-20 20:56:36 +0800

    update

---
+++ spring-jpa31-hibernate617-final/README.md
@@ -0,0 +1,8 @@
+# spring-jpa2.7.12-hibernate6.1.7 Final
+java 11, spring-jar 2.7.12, hibernate 6.1.7 Final
+
+# docker run parameter
+POSTGRES_HOST_AUTH_METHOD=true
+POSTGRES_PASSWORD=password
+POSTGRES_DB=integration-test
+POSTGRES_HOST_AUTH_METHOD=trust
---
+++ spring-jpa31-hibernate617-final/src/test/java/framework_integration_check/app/SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package framework_integration_check.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
